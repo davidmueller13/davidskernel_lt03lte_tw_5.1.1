75f69838f99cf5fa66a030d1cc64f9390a03d1d7
davidmueller13 <david.mueller1013@gmail.com>
2016-02-27 15:50:54 +0100

    sched/fair: Implement fast idling of CPUs when the system is partially loaded
    When a system is lightly loaded (i.e. no more than 1 job per cpu),
    attempt to pull job to a cpu before putting it to idle is unnecessary and
    can be skipped.  This patch adds an indicator so the scheduler can know
    when there's no more than 1 active job is on any CPU in the system to
    skip needless job pulls.
    
    On a 4 socket machine with a request/response kind of workload from
    clients, we saw about 0.13 msec delay when we go through a full load
    balance to try pull job from all the other cpus.  While 0.1 msec was
    spent on processing the request and generating a response, the 0.13 msec
    load balance overhead was actually more than the actual work being done.
    This overhead can be skipped much of the time for lightly loaded systems.
    
    With this patch, we tested with a netperf request/response workload that
    has the server busy with half the cpus in a 4 socket system.  We found
    the patch eliminated 75% of the load balance attempts before idling a cpu.
    
    The overhead of setting/clearing the indicator is low as we already gather
    the necessary info while we call add_nr_running() and update_sd_lb_stats.()
    We switch to full load balance load immediately if any cpu got more than
    one job on its run queue in add_nr_running.  We'll clear the indicator
    to avoid load balance when we detect no cpu's have more than one job
    when we scan the work queues in update_sg_lb_stats().  We are aggressive
    in turning on the load balance and opportunistic in skipping the load
    balance.
    
    Change-Id: I0336c748933d964e9e3e7972ed18d250244eb534
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Acked-by: Rik van Riel <riel@redhat.com>
    Acked-by: Jason Low <jason.low2@hp.com>
    Cc: "Paul E.McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Davidlohr Bueso <davidlohr@hp.com>
    Cc: Alex Shi <alex.shi@linaro.org>
    Cc: Michel Lespinasse <walken@google.com>
    Cc: Peter Hurley <peter@hurleysoftware.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1403551009.2970.613.camel@schen9-DESK
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit 040131fd1aa5eaece660176979e560cdb2b513d6)

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -328,6 +328,9 @@ struct root_domain {
 	cpumask_var_t span;
 	cpumask_var_t online;
 
+	/* Indicate more than one runnable task for any CPU */
+	bool overload;
+
 	/*
 	 * The "RT overload" flag: it gets set if a CPU has more than
 	 * one runnable RT task.
@@ -978,6 +981,11 @@ static inline void inc_nr_running(struct rq *rq)
 #ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_end(&nr_stats->ave_seqcnt);
 #endif
+
+	if (rq->nr_running >= 2) {
+		if (!rq->rd->overload)
+			rq->rd->overload = true;
+	}
 }
 
 static inline void dec_nr_running(struct rq *rq)
